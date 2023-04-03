7d8d603bfd3a0f971ab2f526b7ed9af45a77e5d6
saria-mohi <70873507+saria-mohi@users.noreply.github.com>
2023-04-03 20:17:30 +0200

    try to fix errors

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,52 +1,22 @@
 #!/usr/bin/python3
-# 101-nqueens.py
-"""Program to solve the N queens puzzle challenge"""
+from sys import argv
 
+def main():
+    ac = len(argv)
+    if ac != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    elif argv[1].isdigit() is False:
+        print("N must be a number")
+        exit(1)
+    elif int(argv[1]) < 4:
+        print("N must be at least 4")
+        exit(1)
 
-def isSafe(board, row, col):
-    '''Checks if position is safe from attack.
-    Args:
-        board: The board state.
-        row: The row to check.
-        col: The column to check.
-    '''
-    for c in range(col):
-        if board[c] is row or abs(board[c] - row) is abs(c - col):
-            return False
-    return True
-
-
-def checkBoard(board, col):
-    '''Checks the board state column by column using backtracking.
-    Args:
-        board: The board state.
-        col: The current colum to check.
-    '''
-    n = len(board)
-    if col is n:
-        print(str([[c, board[c]] for c in range(n)]))
-        return
-
-    for row in range(n):
-        if isSafe(board, row, col):
-            board[col] = row
-            checkBoard(board, col + 1)
-
+    size = int(argv[1])
+    chess_board = [["$" for y in range(size)] for x in range(size)]
+    print ( chess_board )
+    print("{:d}".format(int(argv[1])))
 
 if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    n = 0
-    try:
-        n = int(sys.argv[1])
-    except:
-        print("N must be a number")
-        sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    board = [0 for col in range(n)]
-    checkBoard(board, 0)
+    main()
