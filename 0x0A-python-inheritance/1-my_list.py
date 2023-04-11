24a70df45d9374722fad72e14f65e3aacd027ef0
saria-mohi <saria.m.mohammed@gmail.com>
2023-04-11 21:49:17 +0200

    Write a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """ assume that all the elements of the list will be of type 
+    """
+    def print_sorted(self):
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
