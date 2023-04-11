47b8dc524d24fd316144c4ff11f83d94fe7a383f
saria-mohi <saria.m.mohammed@gmail.com>
2023-04-11 22:22:58 +0200

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,7 +6,7 @@ Checks if an attribute can be added to an object.
 
 def add_attribute(an_obj, an_attr, a_value):
     """function that adds a new attribute to an object if it’s possible:
-        Raise a TypeError exception, with the message can't add new attribute if the object can’t have new attribute
+    Raise a TypeError exception, with the message can't add new attribute if the object can’t have new attribute
     Args:
         - an_obj: object to add the attribute to
         - an_attr: name of the attribute
