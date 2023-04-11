5d1095adfd24e267935c83253895b832eb96c72b
saria-mohi <saria.m.mohammed@gmail.com>
2023-04-11 22:02:28 +0200

    lass BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Creates a class.
+"""
+
+
+class BaseGeometry:
+    """lass BaseGeometry (based on 5-base_geometry.py)."""
+
+    def area(self):
+        """Raises an Exception with the message
+        'area() is not implemented'.
+        """
+
+        raise Exception('area() is not implemented')
