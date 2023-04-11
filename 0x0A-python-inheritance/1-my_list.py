2323a19a5d1c69b5a6e5e3fd1dad49447528e8ff
saria-mohi <saria.m.mohammed@gmail.com>
2023-04-11 21:49:57 +0200

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,8 @@ class MyList(list):
     """ assume that all the elements of the list will be of type 
     """
     def print_sorted(self):
+        """Prints the  list
+        """
         new_list = self[:]
         new_list.sort()
         print("{}".format(new_list))
