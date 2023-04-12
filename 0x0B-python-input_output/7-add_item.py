f9e6391d689407ef15e1f88f755c22b0954d491f
saria-mohi <saria.m.mohammed@gmail.com>
2023-04-12 22:23:00 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 
+"""
+Module 9-add_item
 
-import json
+Contains function that adds and saves to Python obj to JSON file; loads objects
+
+"""
 
 
-def save_to_json_file(my_obj, filename):
-    with open(filename, "w", encoding="UTF-8") as f:
-        json.dump(my_obj, f)
+from sys import argv
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+f = "add_item.json"
+
+try:
+    listing = load_from_json_file(f)
+except FileNotFoundError:
+    listing = []
+
+save_to_json_file(listing + argv[1:], f)
