6ffbeb675bfebbe446ba1c89f2b29590f4f68d4f
saria-mohi <saria.m.mohammed@gmail.com>
2023-04-12 18:47:00 +0200

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,8 +5,8 @@ Reads from a file and prints.
 
 
 def read_file(filename=""):
-    """function that reads a text file (UTF8) 
-    and prints it to stdout
+    """function function that reads a text file (UTF8)
+    its contents to stdout.
     Args:
         - filename: name of the file
     """
