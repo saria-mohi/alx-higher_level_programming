51c549c1030a52d4bbba92a63a0ac6a17ef0651e
saria-mohi <saria.m.mohammed@gmail.com>
2023-06-12 21:34:39 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const ArrayLines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i = 0;
-while (ArrayLines[i]) {
-  console.log(ArrayLines[i]);
-  i++;
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
