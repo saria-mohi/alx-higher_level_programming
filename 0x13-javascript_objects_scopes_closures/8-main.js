08bb5f977549dc15ca59b4301d87b645f5d06438
saria-mohi <saria.m.mohammed@gmail.com>
2023-06-13 23:12:12 +0300

    Update 8-main.js

--- 0x13-javascript_objects_scopes_closures/8-main.js
+++ 0x13-javascript_objects_scopes_closures/8-main.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-exports.esrever = function (list) {
-    return list.reduceRight(function (array, current) {
-      array.push(current);
-      return array;
-    }, []);
-  };
+const esrever = require('./8-esrever').esrever;
+
+console.log(esrever([1, 2, 3, 4, 5]));
+console.log(esrever(["School", 89, { id: 12 }, "String"]));
