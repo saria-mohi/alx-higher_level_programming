92cb9f173d852f380c272363b98b0ada07dc86f9
saria-mohi <saria.m.mohammed@gmail.com>
2023-06-30 23:18:35 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
-"""Find peak in list of unsorted integers
-"""
+"""task 6 modele"""
 
 
 def find_peak(list_of_integers):
-    """Find peak in unsorted list
-    """
-    loi = list_of_integers
-    size = len(loi)
-
-    if size == 0:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
 
-    if size is 1:
-        return loi[0]
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
 
-    return recurse(loi, 0, size - 1)
-
-
-def recurse(loi, left, right):
-    """Recursive component
-    """
-    mid = int((left + right) / 2)
-
-    if left > right:
-        return loi[mid]
-
-    if (mid == 0 or loi[mid] > loi[mid - 1])\
-       and (mid == len(loi) - 1 or loi[mid] > loi[mid + 1]):
-        return loi[mid]
-
-    # recurse left
-    elif (mid > 0) and loi[mid - 1] > loi[mid]:
-        return recurse(loi, left, mid - 1)
-    else:  # recurse right
-        return recurse(loi, mid + 1, right)
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
