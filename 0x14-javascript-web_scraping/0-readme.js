2e19b482b9a82463addae0219cd11b1c1319d186
saria2016 <saria.m.mohammed@gmail.com>
2023-07-26 19:31:07 +0300

    read data from file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,15 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file.
-const file = require { "file" }
+const fs = require('fs');
+const file = process.argv[2];
 
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
 
 
 
