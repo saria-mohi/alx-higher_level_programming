e5e22328f4f7aa3a485b7d11bc8fafa028164252
saria2016 <saria.m.mohammed@gmail.com>
2023-07-26 22:05:08 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,9 +2,8 @@
 // script that computes the number of tasks completed by user id.
 
 const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, response, body) {
+request(process.argv[2], function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
