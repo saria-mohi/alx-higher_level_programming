a4ba6ec6cc0a71405e1bdf21233fb97734c6c0c8
saria2016 <saria.m.mohammed@gmail.com>
2023-07-26 19:43:36 +0300

    read data from file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const file = process.argv[2];
 
+// read data from file or print error if not exist
 fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
