57d3738e0960a1dfc12cfbf5a16fc516fadc0674
saria2016 <saria.m.mohammed@gmail.com>
2023-07-26 21:57:22 +0300

    fix identation

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,13 +6,15 @@ const request = require('request');
 // const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
-function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const responseJSON = JSON.parse(body);
-    console.log(responseJSON.title);
-  } else {
-    console.log('Error code: ' + response.statusCode);
-  }
-});
+    function (err, response, body) {
+      if (err) {
+        console.log(err);
+      }
+      else if (response.statusCode === 200) {
+        const responseJSON = JSON.parse(body);
+        console.log(responseJSON.title);
+      }
+      else {
+        console.log('Error code: ' + response.statusCode);
+      }
+    });
