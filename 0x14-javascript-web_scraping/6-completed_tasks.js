ae50321c3a13751752c3ba902c43d0d1a9e54262
saria2016 <saria.m.mohammed@gmail.com>
2023-07-26 23:19:33 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,7 +7,7 @@ request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    const completed = [];
+    const completed = {};
     const tasks = JSON.parse(body);
     for (const i in tasks) {
       const task = tasks[i];
