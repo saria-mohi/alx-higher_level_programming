b4cb67618f40c432bea3d88557d658a91de4107c
saria2016 <saria.m.mohammed@gmail.com>
2023-07-26 22:10:07 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,25 +2,24 @@
 // script that computes the number of tasks completed by user id.
 
 const request = require('request');
+const url = process.argv[2];
 
-request(process.argv[2], function (err, response, body) {
+request(url, function (err, response, body) {
   if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
-      }
-    }
-    console.log(completed);
+    throw new Error(err);
   } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    const userCompletedTasks = {};
+    newBody = JSON.parse(body);
+
+    for (let i = 0; i < newBody.length; i++) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !userCompletedTasks[userId]) {
+        userCompletedTasks[userId] = 0;
+      }
+       if (completed) ++userCompletedTasks[userId];
+    }
+    console.log(userCompletedTasks);
   }
 });
